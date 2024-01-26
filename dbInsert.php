a35d3015392aed9c03302784d66008e2cde6c750
fredrickomondi <omondifredrick@gmail.com>
2024-01-26 15:52:30 +0000

    Update dbInsert.php
    check commented text

--- dbInsert.php
+++ dbInsert.php
@@ -3,7 +3,7 @@
 $fullName = $_POST['fullName'];
 $email = $_POST['email'];
 $studentPassword = $_POST['studentPassword'];
-$confirm_password = $_POST['confirm_password'];
+$confirm_password = $_POST['confirm_password']; // need to stick to one convention for variable naming, camelCase or snake_case.
 $admNumber= $_POST['admNumber'];
 $gender =$_POST['gender'];
 $progLang =$_POST['progLang'];
@@ -13,7 +13,7 @@ $progLang =$_POST['progLang'];
 $servername="localhost";
 $dbUsername= "root";
 $dbPassword= "";
-$dbname= "school_records";
+$dbname= "school_records"; //snake case variable
 
 $conn = new mysqli($servername, $dbUsername, $dbPassword, $dbname);
 
@@ -30,7 +30,8 @@ VALUES ('$fullName','$email', '$studentPassword', '$confirm_password', '$admNumb
 
 if(mysqli_query($conn, $sql)) {
     echo "Thank you for providing your details";
-     header("location: http://localhost/StudentsProject/");
+     header("location: http://localhost/StudentsProject/"); // you shouldnt use full url i.e "http://localhost/StudentsProject/" just use the relative paths
+    //you can read more about relative paths here <https://www.codingrooms.com/blog/file-paths>
 }
 else {
     echo "Uurg, we may have encountered a problem ". $sql. "<br>". mysqli_error( $conn);
